0444db1700406ec69a764bcf13cd6685a7c717b2
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2023-02-13 16:39:46 +0900

    commit for initialization

---
+++ src/main/java/com/boilerplate/hackathonboilerplate/HackathonBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.boilerplate.hackathonboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonBoilerplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boilerplate/hackathonboilerplate/HackathonBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boilerplate.hackathonboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
