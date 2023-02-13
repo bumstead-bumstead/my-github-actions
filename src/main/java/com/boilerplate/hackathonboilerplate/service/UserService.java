ff743c57f8350e3bde52ff0b8b9391c613eaa05a
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2023-02-13 17:56:10 +0900

    create UserService
     - add signUp method

--- src/main/java/com/boilerplate/hackathonboilerplate/service/UserService.java
+++ src/main/java/com/boilerplate/hackathonboilerplate/service/UserService.java
@@ -1,2 +1,27 @@
-package com.boilerplate.hackathonboilerplate.service;public class UserService {
+package com.boilerplate.hackathonboilerplate.service;
+
+
+import com.boilerplate.hackathonboilerplate.domain.User;
+import com.boilerplate.hackathonboilerplate.domain.UserDto;
+import com.boilerplate.hackathonboilerplate.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public UserDto.signUpResponse signUp(UserDto.signUpRequest userDto) {
+        User user = userDto.toEntity();
+        user.setCreatedDate(LocalDateTime.now());
+        User savedUser = userRepository.save(user);
+
+        return UserDto.signUpResponse.builder()
+                .id(savedUser.getId())
+                .createdDate(savedUser.getCreatedDate())
+                .name(savedUser.getName()).build();
+    }
 }
