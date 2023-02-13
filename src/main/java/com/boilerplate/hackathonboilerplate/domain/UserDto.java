22501924d43592a946368e3994de5d4fa167b6b7
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2023-02-13 17:55:46 +0900

    refactor UserDto

--- src/main/java/com/boilerplate/hackathonboilerplate/domain/UserDto.java
+++ src/main/java/com/boilerplate/hackathonboilerplate/domain/UserDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class UserDto {
 
     @Getter
@@ -13,14 +17,34 @@ public class UserDto {
     public static class Info {
         private int id;
         private String name;
-        private int age;
+        private String password;
+        private LocalDate birth;
     }
 
     @Getter
     @Setter
-    public static class Request {
+    public static class FindRequest {
         private String name;
-        private int age;
+    }
+    @Getter
+    @Setter
+    public static class signUpRequest {
+        private String name;
+        private String password;
+        private LocalDate birth;
+
+        public User toEntity() {
+            return User.builder().password(password).name(name).birth(birth).build();
+        }
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    public static class signUpResponse {
+        private String name;
+        private Integer id;
+        private LocalDateTime createdDate;
     }
 
     @Getter
