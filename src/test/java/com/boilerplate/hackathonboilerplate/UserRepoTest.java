5efb35ab7bacd38019b8541b9bfecf28377773f0
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2023-02-14 23:17:49 +0900

    영속계층 단위 테스트를 위해 @DataJpaTest로 설정

--- src/test/java/com/boilerplate/hackathonboilerplate/UserRepoTest.java
+++ src/test/java/com/boilerplate/hackathonboilerplate/UserRepoTest.java
@@ -6,13 +6,14 @@ import com.boilerplate.hackathonboilerplate.domain.User;
 import com.boilerplate.hackathonboilerplate.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@SpringBootTest
+@DataJpaTest
 public class UserRepoTest {
     @Autowired
     UserRepository userRepository;
